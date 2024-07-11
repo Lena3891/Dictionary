71759b6e3543dbbd814a8a3ec0d7f8178d8a7162
Lena3891 <164846971+Lena3891@users.noreply.github.com>
2024-07-11 14:02:11 +0200

    Added API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,24 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
     let [keyword, setKeyword]= useState("");
+
+    function handleResponse(response) {
+      console.log(response.data); 
+    }
+
     function search(event) {
         event.preventDefault();
-      alert(`Searching for ${keyword} definition`);
+     
+
+       let apiKey = "8f89013d30bfc04f0f041a1bdo2t3fe7";
+       let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={word}&key={key}`;
+       axios.get(apiUrl).then(handleResponse);
     }
+   
+
     function handleKeywordChange(event) {
       setKeyword(event.target.value);
     }
