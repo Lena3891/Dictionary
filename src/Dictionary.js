43aa77c8a1d288e008cf03c4cbb0ec942c03ed16
Lena3891 <164846971+Lena3891@users.noreply.github.com>
2024-07-11 15:18:59 +0200

    Changed API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,33 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 
 export default function Dictionary() {
-    let [keyword, setKeyword]= useState("");
+  let [keyword, setKeyword] = useState("");
 
-    function handleResponse(response) {
-      console.log(response.data); 
-    }
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
-    function search(event) {
-        event.preventDefault();
-     
+  function search(event) {
+    event.preventDefault();
 
-       let apiKey = "8f89013d30bfc04f0f041a1bdo2t3fe7";
-       let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={word}&key={key}`;
-       axios.get(apiUrl).then(handleResponse);
-    }
-   
+    
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/hello`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-    function handleKeywordChange(event) {
-      setKeyword(event.target.value);
-    }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-    return (
+  return (
     <div className="Dictionary">
-    <form onSubmit={search}>
-       <input type="search" onChange={handleKeywordChange}/>
-    </form>
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
     </div>
-   );
+  );
 }
