1d03da1fe637377808a199cb0bcdf3a1972ef8a5
Lena3891 <164846971+Lena3891@users.noreply.github.com>
2024-07-13 11:57:19 +0200

    Added components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
         <main>
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Magdalena Zyglewicz</small>
+          <small>Coded by Magdalena Zyglewicz and is open-sourced on <a href="https://github.com/Lena3891/Dictionary" target="_blank">GitHub</a>{" "}and hostet on <a href="">Netlify</a></small>
         </footer>
       </div>
     </div>
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,22 +1,21 @@
 import React from "react";
+import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-    console.log(props.meaning);
-    return (
-      <div className="Meaning">
-        <h3>{props.meaning.partOfSpeech}</h3>
-        {props.meaning.definitions.map(function(definition, index) {
-          return (
-            <div key={index}>
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <div className="definition">{definition.definition}</div>
 
-              <p>
-                {definition.definition}
-                <br />
-                <em>{definition.example}</em>
-              </p>
-            </div>
-          );  
-        })}
-      </div>
-    );
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
